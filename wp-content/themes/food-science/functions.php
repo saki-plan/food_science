76b3886960018a95570b500666a77384bd9d6c5a
taro shinjuku <12345@gmail.com>
2024-12-06 08:58:30 +0900

    投稿一覧　カテゴリーページ迄作成

--- wp-content/themes/food-science/functions.php
+++ wp-content/themes/food-science/functions.php
@@ -2,12 +2,25 @@
 /**
  * タイトルタグを出力する
  */
-
 add_theme_support('title-tag');
+
 /**
- * 
+ * アイキャッチ画像を使用可能にする
+ */
+add_theme_support('post-thumbnails');
+
+
+/**
+ * タイトルの区切り文字を変更
  */
 add_filter('document_title_separator', 'my_document_title_separator');
 function my_document_title_separator() {
     return '|';
 }
+/**
+ * 画面下部のエラー表示をとりあえず消す方法
+ * Notice: ob_end_flush(): failed to send buffer of zlib output compression (1) in
+ * 原因はデータ圧縮などを行うzlibというライブラリがサーバーにインストールされていないか、
+ * ライブラリ自体に問題が起きている可能性。
+ */
+remove_action( 'shutdown', 'wp_ob_end_flush_all', 1 );
